c41668adf4e4ce30e10fe615b8b65c825acdc4b3
Raul Madero <raul.madero.ramirez@gmail.com>
2025-04-24 13:48:12 -0600

    Registro de usuarios

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -22,6 +23,14 @@ class RegisterController extends Controller
             'user' => 'required|min:3|max:20|unique:users,user',
             'email' => 'required|email|unique:users,email',
             'password' => 'required|min:6|max:20|confirmed'
+        ]);
+
+        // dd('Creando usuario...');
+        User::create([
+            'name' => $request->get('name'),
+            'user' => $request->get('user'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->get('password')),
         ]);
     }
 }
